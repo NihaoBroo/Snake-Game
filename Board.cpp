52f06da1653b9567bd14ff279553d2113d73d468
NihaoBroo <nihaobro00@gmail.com>
2024-10-22 21:59:28 +0300

    Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -9,3 +9,27 @@ int GameBoard::Width() const {
 int GameBoard::Height() const {
     return boardHeight;
 }
+Board Board::operator=(const Board &other)
+{
+    _width = other.GetWidth();
+    _height = other.GetHeight();
+    return *this;
+}
+
+bool Board::operator==(const Board &other) const
+{
+    return GetWidth() == other.GetWidth() && GetHeight() == other.GetHeight();
+}
+
+std::istream &operator>>(std::istream &in, Board &board)
+{
+    int width, height;
+    in >> width >> height;
+    board = Board(width, height);
+    return in;
+}
+
+std::ostream &operator<<(std::ostream &out, const Board &board)
+{
+    out << board.GetWidth() << " " << board.GetHeight();
+    return out;
