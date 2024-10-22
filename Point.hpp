e9ad0e20c691e679055baafe01a862a3c3d0f535
NihaoBroo <nihaobro00@gmail.com>
2024-10-22 21:50:29 +0300

    Point.hpp

--- Point.hpp
+++ Point.hpp
@@ -6,4 +6,10 @@
 
     Point() : x(0), y(0) {}  
     Point(int xPos, int yPos) : x(xPos), y(yPos) {}  
+  
+    Point operator=(const Point& other);  
+    bool operator==(const Point& other) const;  
+
+    friend std::istream& operator>>(std::istream& in, Point& point);  
+    friend std::ostream& operator<<(std::ostream& out, const Point& point);  
 };
