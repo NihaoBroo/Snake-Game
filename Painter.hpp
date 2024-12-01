157841b702d24db82ae914ebb8922465c79f62d0
NihaoBroo <nihaobro00@gmail.com>
2024-12-01 20:31:56 +0200

    Painter.hpp

--- Painter.hpp
+++ Painter.hpp
@@ -1,10 +1,15 @@
 #pragma once
 
-#include "abstract_painter.hpp"
+struct Point {
+    int x;
+    int y;
 
-class Painter : public AbstractPainter {
-public:
-    void DrawImage(const Point& topLeft, const Point& bottomRight, char** image) override; 
-    void WriteText(const Point& position, const char* text) override;                     
+    Point() : x(0), y(0) {}
+    Point(int xPos, int yPos) : x(xPos), y(yPos) {}
+
+    Point operator=(const Point& other);
+    bool operator==(const Point& other) const;
+
+    friend std::istream& operator>>(std::istream& in, Point& point);
+    friend std::ostream& operator<<(std::ostream& out, const Point& point);
 };
- 
